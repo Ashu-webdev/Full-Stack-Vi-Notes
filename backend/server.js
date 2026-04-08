e454b4a49ba3229269b56b3b8e6f7d628a04dc7a
Ashutosh Rout <ashutoshrout1729@gmail.com>
2026-04-08 22:30:44 +0530

    Updated DB connection and env setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-connectDB();
-
 app.listen(5000, () => {
   console.log("Server running on port 5000");
+});
+
+connectDB().catch((err) => {
+  console.error("Failed to connect to MongoDB", err);
+  process.exit(1);
 });
